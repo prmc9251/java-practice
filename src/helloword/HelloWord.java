a505aa99af72de4aa98a4e29b074ae74806af02e
prmc9251 <prmc.9251@gmail.com>
2025-10-25 14:59:39 +0100

    Java Practice project created

---
+++ src/helloword/HelloWord.java
@@ -0,0 +1,10 @@
+package helloword;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
