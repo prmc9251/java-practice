67282e18ce86526099518c87955ca87d7efb20e3
prmc9251 <prmc.9251@gmail.com>
2025-11-08 09:37:48 +0000

    Challenge3

---
+++ src/morningchallenge/Main.java
@@ -0,0 +1,14 @@
+package morningchallenge;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Challenge3 emp1 = new Challenge3(101, 10, "John", "Manual Tester");
+        emp1.display();
+        Challenge3 emp2 = new Challenge3(102, 11, "Smith", "Automation tester");
+        emp2.display();
+    }
+}
